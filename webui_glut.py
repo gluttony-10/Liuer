f4305781a7b638cc90afd572d8320d3c611d36d1
gluttony-10 <326581344@qq.com>
2025-02-24 23:17:43 +0800

    Add onnx

--- webui_glut.py
+++ webui_glut.py
@@ -2,18 +2,17 @@
 
 import os
 import gradio as gr
-import numpy as np
 import torch
-import torchaudio
 from funasr import AutoModel
+from funasr_onnx import SeacoParaformer, CT_Transformer
 import gc
-import json
-from pathlib import Path
+
+os.environ["MODELSCOPE_CACHE"] = ".glut/.modelscope"
 
 class FunASRApp:
     def __init__(self):
         self.model = None
-        self.hotword = self._load_hotwords()
+        self.hotwords = self._load_hotwords()
 
 
     def _load_hotwords(self):
@@ -46,9 +45,16 @@ class FunASRApp:
             spk_model = None
             model_name = "iic/SenseVoiceSmall"
             model = "情感模型"
-        elif "热词" in model:
+        elif model == "热词模型":
             model_name = "iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
-            model = "热词模型"
+        elif model == "热词模型onnx版":
+            model_name = "iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
+            model_punc = "iic/punc_ct-transformer_cn-en-common-vocab471067-large"
+            self.model = SeacoParaformer(model_name, batch_size=1, quantize=True)
+            self.model2 = CT_Transformer(model_punc, batch_size=1, quantize=True)
+            print(f'\033[32m{model}加载成功\033[0m')
+            return f"{model}加载完成", gr.update(interactive=True)
+        
         print(f'\033[32m开始加载{model}\033[0m')
         self.model = AutoModel(
             model=model_name,  
@@ -68,7 +74,7 @@ class FunASRApp:
             model,
             video_input,
             language,
-            hotword,
+            hotwords,
             format_selector,
             save_button,
             use_itn=True,
@@ -77,6 +83,13 @@ class FunASRApp:
             merge_length_s=15,
             sentence_timestamp=True,
     ):        
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
+
+        with open("hotwords.txt", "w") as f:
+            f.write(hotwords)
+
         if "选择" in model:
             print(f'\033[31m请先选择加载模型\033[0m')
             return "请先选择加载模型", "请先选择加载模型"
@@ -86,15 +99,19 @@ class FunASRApp:
                 return "情感模型仅支持TXT格式", "情感模型仅支持TXT格式"
             sentence_timestamp = False
             model = "情感模型"
-        elif "热词" in model:
+        elif model == "热词模型":
             model = "热词模型"
-        
-        with open("hotwords.txt", "w") as f:
-            f.write(hotword)
+        elif model == "热词模型onnx版":
+            for input in video_input:
+                asr_result = self.model(
+                input,
+                hotwords=hotwords
+                )
+                punc_result = self.model2(asr_result[0]['preds'])
+                res = self.merge_asr_and_punc(asr_result, punc_result, input)
+                status_text, content = self.process_result(res, input, format_selector, save_button, model)
+            return status_text, content
 
-        gc.collect()
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
         language_abbr = {"自动": "auto", "中文": "zh", "英文": "en", "粤语": "yue", "日文": "ja", "韩文": "ko", "无语言": "nospeech"}
         language = "自动" if len(language) < 1 else language
         language = language_abbr[language]
@@ -109,39 +126,109 @@ class FunASRApp:
                 merge_vad=merge_vad,
                 merge_length_s=merge_length_s,
                 sentence_timestamp=sentence_timestamp,
-                hotword=hotword,
+                hotwords=hotwords,
             )
-            #print(res) # 输出原始识别结果
+            status_text, content = self.process_result(res, input, format_selector, save_button, model)
+        print(res)
+        return status_text, content
+    
 
-            output_dir = "outputs"
-            os.makedirs(output_dir, exist_ok=True)
-            filename = os.path.basename(input)
-            filename_without_extension, extension = os.path.splitext(filename)
-            
-            # 根据格式生成内容
-            base_path = os.path.join(output_dir, filename_without_extension)
-            if format_selector == "LRC":
-                content = self._generate_lrc(res)
-                if save_button:
-                    with open(f"{base_path}.lrc", "w") as f:
-                        f.write(content)
-            elif format_selector == "SRT":
-                content = self._generate_srt(res)
-                if save_button:
-                    with open(f"{base_path}.srt", "w") as f:
-                        f.write(content)
-            else:
-                content = res[0]["text"]
-                if format_selector:
-                    with open(f"{base_path}.txt", "w") as f:
-                        f.write(content)
-
-            status_text = f"{model}识别成功"
-            if save_button:
-                status_text += f"，{format_selector}文件已保存至{output_dir}"
-            else:
-                status_text += "，未选择保存文件"
+    def merge_asr_and_punc(self, asr_result, punc_result, audio_key):
+        # 提取 asr_result 中的信息
+        timestamps = asr_result[0]['timestamp']
         
+        # 提取 punc_result 中的信息
+        original_text = asr_result[0]['preds'].split()
+        punctuated_text = punc_result[0]
+        
+        # 根据标点符号分割文本和时间戳
+        sentences = []
+        sentence_timestamps = []
+        start_index = 0
+        n = 1
+        
+        # 定义标点符号
+        punctuation_marks = "，。！？；："
+        
+        for i, char in enumerate(punctuated_text):
+            if char in punctuation_marks:
+                end_index = i
+                sentence = original_text[start_index:end_index-n+1] + [char]
+                print(sentence)
+                sentence_timestamp = timestamps[start_index:(end_index-n+1)]
+                sentences.append(sentence)
+                sentence_timestamps.append(sentence_timestamp)
+                start_index = end_index-n+1
+                n += 1
+        
+        # 处理最后一个句子（如果没有以标点符号结尾）
+        if start_index < len(punctuated_text):
+            sentence = punctuated_text[start_index:]
+            sentence_timestamp = timestamps[start_index:]
+            sentences.append(sentence)
+            sentence_timestamps.append(sentence_timestamp)
+        
+        # 构建最终的输出格式
+        sentence_info = []
+        
+        for sentence, timestamp in zip(sentences, sentence_timestamps):
+            if timestamp:  # 确保 timestamp 不为空
+                start_time = timestamp[0][0]
+                end_time = timestamp[-1][1]
+                sentence_info.append({
+                    'text': sentence,
+                    'start': start_time,
+                    'end': end_time,
+                    'timestamp': timestamp,
+                    'spk': 0
+                })
+        
+        output = [{
+            'key': audio_key,
+            'text': punctuated_text,
+            'timestamp': [ts for sublist in sentence_timestamps for ts in sublist],
+            'sentence_info': sentence_info
+        }]
+        
+        return output
+    
+
+    def process_result(
+            self, 
+            res, 
+            input, 
+            format_selector, 
+            save_button, model
+    ):
+        output_dir = "outputs"
+        os.makedirs(output_dir, exist_ok=True)
+        filename = os.path.basename(input)
+        filename_without_extension, extension = os.path.splitext(filename)
+        
+        # 根据格式生成内容
+        base_path = os.path.join(output_dir, filename_without_extension)
+        if format_selector == "LRC":
+            content = self._generate_lrc(res)
+            if save_button:
+                with open(f"{base_path}.lrc", "w") as f:
+                    f.write(content)
+        elif format_selector == "SRT":
+            content = self._generate_srt(res)
+            if save_button:
+                with open(f"{base_path}.srt", "w") as f:
+                    f.write(content)
+        else:
+            content = res[0]["text"]
+            if format_selector:
+                with open(f"{base_path}.txt", "w") as f:
+                    f.write(content)
+
+        status_text = f"{model}识别成功"
+        if save_button:
+            status_text += f"，{format_selector}文件已保存至{output_dir}"
+        else:
+            status_text += "，未选择保存文件"
+
         return status_text, content
 
 
@@ -150,7 +237,11 @@ class FunASRApp:
         lrc_lines = []
         for segment in res[0]["sentence_info"]:
             start = self._format_lrc_time(segment.get("start", 0.0))
-            text = segment.get("text", "").strip()
+            text = segment.get("text", "")
+            # 添加类型转换
+            if isinstance(text, list):
+                text = "".join(text)
+            text = text.strip()
             # 简化为只显示开始时间
             lrc_lines.append(f"[{start}]{text}") 
         return "\n".join(lrc_lines)
@@ -172,7 +263,11 @@ class FunASRApp:
             # 时间格式转换（新增带小时的三段式格式）
             start = self._format_srt_time(segment.get("start", 0.0))
             end = self._format_srt_time(segment.get("end", 0.0))
-            text = segment.get("text", "").strip()
+            text = segment.get("text", "")
+            # 添加类型转换
+            if isinstance(text, list):
+                text = "".join(text)
+            text = text.strip()
             
             # 构建字幕块
             srt_lines.append(f"{index}")
@@ -206,7 +301,7 @@ class FunASRApp:
                     with gr.Accordion(label="配置"):
                         model_inputs = gr.Dropdown(
                             label="模型", 
-                            choices=["热词模型", "情感模型", "请先选择加载模型"], 
+                            choices=["热词模型", "热词模型onnx版", "情感模型", "请先选择加载模型"], 
                             value="请先选择加载模型"
                         )
                         language_inputs = gr.Dropdown(
@@ -220,7 +315,7 @@ class FunASRApp:
                             value="TXT"
                         )
                         save_button = gr.Checkbox(label="保存文件", value=False)
-                        hotword_inputs = gr.Textbox(label="热词", value=self.hotword)
+                        hotwords_inputs = gr.Textbox(label="热词", value=self.hotwords)
                     fn_button = gr.Button("开始", variant="primary")
                 with gr.Column():
                     status_text = gr.Textbox(label="状态", value="请先选择加载模型", interactive=False)
@@ -236,7 +331,7 @@ class FunASRApp:
                     model_inputs,
                     video_input,
                     language_inputs, 
-                    hotword_inputs,
+                    hotwords_inputs,
                     format_selector,
                     save_button
                 ],
